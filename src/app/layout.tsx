f7fc3ce645e5beedd19f0bf14b8d257ad3a4a693
Vinícius Marcili <vinipetter@gmail.com>
2024-07-18 12:56:34 -0300

    feat: Add footer component and update layout
    This commit adds a new `Footer` component to the application and updates the `RootLayout` and `LoginLayout` components to include the `Footer` component. The `Footer` component displays the developer's name and a link to their GitHub profile. This change improves the overall user experience by providing consistent navigation and branding across the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import Footer from "@/components/footer/footer"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -15,8 +16,11 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br" className='w-full h-full'>
+      <body className={`${inter.className} w-full h-full min-h-screen flex flex-col`}>
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
