28d64427082c4d00615f619cc4813c4f1f535bde
Vinícius Marcili <vinipetter@gmail.com>
2024-10-08 22:10:04 -0300

    refactor: Improve error handling in login form and update address form component and page layout

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,41 +1,39 @@
-
 import { NextRequest, NextResponse } from "next/server"
 
-// Interface de resposta da rota
 type LoginResponse = {
   token?: string
   message?: string
 }
 
-// Interface do corpo da requisição
 interface LoginBody {
   email: string
   password: string
 }
 
-// Função que será executada quando a rota for chamada atravéz do método POST
 export async function POST(request: NextRequest): Promise<NextResponse<LoginResponse>> {
   try {
-    // Lê o corpo da requisição
     const body: LoginBody = await request.json()
     const { email, password } = body
 
-    // Verifica se o email e senha foram informados
     if (!email || !password) {
-      return NextResponse.json({ message: 'Email and password are required' }, { status: 400 })
+      return NextResponse.json({
+        message: 'E-mail e senha são obrigatórios.'
+      }, { status: 400 })
     }
 
-    // TODO: Implementar a lógica de autenticação
+    // TODO: Implementar lógica de autenticação
     if (email === 'email@teste.com' && password === '123456') {
-      return NextResponse.json({ token: 'fake-token' })
+      return NextResponse.json({ token: 'token' })
     }
 
-    // Retorna erro de autenticação
-    return NextResponse.json({ message: 'Invalid email or password' }, { status: 401 })
+    return NextResponse.json({
+      message: 'E-mail ou senha inválidos.'
+    }, { status: 401 })
   } catch (error) {
     console.error(error)
-    // Retorna erro interno do servidor
-    return NextResponse.json({ message: 'Internal server error' }, { status: 500 })
+
+    return NextResponse.json({
+      message: 'Internal Server Error'
+    }, { status: 500 })
   }
 }
-
--- src/app/dashboard/address/page.tsx
+++ src/app/dashboard/address/page.tsx
@@ -1,12 +1,12 @@
 import AddressForm from "./_components/address-form/address-form.component"
 
 const AddressPage = () => {
-  return <>
-    <div className='p-3 rounded mx-auto w-full h-full flex ' style={{ maxWidth: '500px' }}>
+  return (
+    <div className='p-3 rounded mx-auto w-full h-full flex flex-col items-center justify-center' style={{ maxWidth: '500px' }}>
       <h6 className="text-2xl font-semibold text-center mb-4">Address Page</h6>
       <AddressForm />
     </div>
-  </>
+  )
 }
 
 export default AddressPage
