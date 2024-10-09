70f3fc20ff6906d8d7aa73e91318f984d511b087
Vinícius Marcili <vinipetter@gmail.com>
2024-10-08 21:15:01 -0300

    feat: Add ViaCep client and address form component
    This commit adds a new file, `viacep.client.ts`, which contains the `getZipcode` function for fetching address data from the ViaCep API. It also introduces a new address form component, `AddressForm`, which handles the submission and validation of address data.
    
    The `viacep.client.ts` file defines the `ViaCepResponse` type and exports the `getZipcode` function. This function takes a `zipcode` parameter and makes a request to the ViaCep API to fetch address data. It handles error cases such as invalid zipcodes and non-existent addresses.
    
    The `AddressForm` component is a form that allows users to enter their address details. It uses the `useForm` hook to manage form state and validation. The form includes fields for the zipcode, street, number, complement, neighborhood, city, state, and country. It also includes a submit button that triggers the `submitCallback` function to send the address data to the server.
    
    This commit lays the foundation for the address functionality in the dashboard.

---
+++ src/app/dashboard/address/page.tsx
@@ -0,0 +1,12 @@
+import AddressForm from "./_components/address-form/address-form.component"
+
+const HomeDashboardPage = () => {
+  return <>
+    <h6 className="text-center" style={{ minHeight: 'calc(100vh - 36px)' }}>
+      Address Page
+    </h6>
+    <AddressForm />
+  </>
+}
+
+export default HomeDashboardPage
