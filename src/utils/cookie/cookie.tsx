46a3402d85e91d4ed8f2273a1cf2974b538f2309
Vinícius Marcili <vinipetter@gmail.com>
2024-10-15 19:21:53 -0300

    Refactor cookie utility functions

---
+++ src/utils/cookie/cookie.tsx
@@ -0,0 +1,26 @@
+export function getCookie(name: string) {
+  const value = `; ${document.cookie}`
+  const parts = value.split(`; ${name}=`)
+  if (parts.length === 2) {
+    return parts.pop()?.split(';').shift()
+  }
+  return undefined
+}
+
+export function setCookie(name: string, value: string, days?: number) {
+  let cookieString = `${encodeURIComponent(name)}=${encodeURIComponent(value)}`
+
+  if (days) {
+    const date = new Date()
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
+    cookieString += `; expires=${date.toUTCString()}`
+  }
+
+  document.cookie = cookieString
+}
+
+export function deleteCookie(name: string, path: string, domain: string) {
+  if (getCookie(name)) {
+    document.cookie = `${name}=; path=${path}; domain=${domain}; expires=Thu, 01 Jan 1970 00:00:01 GMT`
+  }
+}
