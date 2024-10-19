f2beaec8b8c8904ecb94aeb3785d2e0bf83d5322
Vinícius Marcili <vinipetter@gmail.com>
2024-10-19 18:21:02 -0300

    Finish new Layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation" // Importa a função redirect, que p
 import { ReactNode } from "react" // Importa o tipo ReactNode, que representa qualquer elemento válido no React.
 import ButtonLogout from "./(components)/button-logout.component" // Importa o componente ButtonLogout, responsável por lidar com a funcionalidade de logout.
 import Link from "next/link"
+import DashboardHeader from "./(components)/header.component"
 
 interface DashboardLayoutProps {
   children: ReactNode // Define a propriedade "children" como um ReactNode, que representa o conteúdo passado para este componente.
@@ -17,17 +18,12 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   }
 
   // Renderiza o layout do dashboard. Caso o token seja válido, o conteúdo é exibido normalmente.
-  return <div className="flex flex-col gap-1 content-center h-full">
-    <header className="sticky top-0 bg-black text-white flex items-center justify-between px-4 py-2">
-      <h1 className="text-xl">
-        <Link href="/dashboard">Dashboard</Link>
-      </h1>
-      <ButtonLogout /> {/* Componente de botão de logout */}
-    </header>
-    <main className="px-2 flex items-center justify-center h-full">
-      {children} {/* Exibe o conteúdo (children) passado para o layout */}
-    </main>
-  </div>
+  return <div className="flex flex-col md:flex-row h-full">
+  <DashboardHeader />
+  <main className="flex-1 px-4 py-2 flex items-center justify-center bg-gray-100">
+    {children}
+  </main>
+</div>
 }
 
 export default DashboardLayout // Exporta o componente DashboardLayout para ser utilizado em outras partes da aplicação.
